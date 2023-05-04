68ec6782d2d82dca33acac0ff152117c99ad221e
oliver <2799274183@qq.com>
2023-05-04 10:41:14 +0800

    success!

--- lualib/my_modules/test.lua
+++ lualib/my_modules/test.lua
@@ -5,7 +5,7 @@ local css_content = [=[
         .scrollnews_li{background:url(images/dot.jpg);background-position: 0px 24px;background-repeat:repeat-x;}
         .scrollnews3_ul li{background:url(images/dot.jpg);background-position: 0px 24px;background-repeat:repeat-x;padding-left:10px;float:left;}
         @import url("cms-ie6.min.css");
-
+        ndex-titlebg{background: url(Index_TitleBg.png) no-re
 
 ]=]
 
@@ -41,11 +41,12 @@ end
 
 local replaced_urls = {}
 for _, url_info in ipairs(url_infos) do
-    local replaced_url = url_info.url..'qqqqqqqqqqqqqqqqqqqqqq'
+    local replaced_url = 'qqqqqqqqqqqqqqqqqqqqqq'
     replaced_urls[url_info.url] = replaced_url
 end
 
 for original_url, replaced_url in pairs(replaced_urls) do
+    print('original_url:',original_url, 'replaced_url:',replaced_url)
     css_content = string.gsub(css_content, escape(original_url), replaced_url)
 end
 
